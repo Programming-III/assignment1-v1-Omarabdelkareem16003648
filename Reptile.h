0af549493da9f5aa4723b5a24db3551d61debb26
Omarabdelkareem16003648 <omar.gado@student.giu-uni.de>
2025-11-12 15:09:23 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -3,7 +3,21 @@
 
 #include "Animal.h"
 
-//define reptile class here
+class Reptile :{
+private:
+    bool isVenomous;
+
+public:
+    Reptile(string n, int a, bool hungry, bool venom)
+        : Animal(n, a, hungry), isVenomous(venom) {}
+
+    void display() const override {
+        cout << "Animal Type: Reptile" << endl;
+        Animal::display();
+        cout << "Venomous: " << (isVenomous ? "Yes" : "No") << endl;
+       
+    }
+};
 
 
 
