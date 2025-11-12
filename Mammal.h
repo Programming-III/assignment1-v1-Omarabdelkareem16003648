7869a59575b6b9b5af473613af5c2ee67651e262
Omarabdelkareem16003648 <omar.gado@student.giu-uni.de>
2025-11-12 15:23:14 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -17,23 +17,10 @@ public:
       
     }
 };
+#endif
 
 
-class Bird : public Animal {
-private:
-    float wingSpan;
 
-public:
-    Bird(string n, int a, bool hungry, float ws)
-        : Animal(n, a, hungry), wingSpan(ws) {}
-
-    void display()   {
-        cout << "Animal Type: Bird" << endl;
-        Animal::display();
-        cout << "Wing Span: " << wingSpan << " meters" << endl;
-        
-    }
-};
 
 
 
