757b8ab48e93eefb86616970e44468b6c3fdda37
Omarabdelkareem16003648 <omar.gado@student.giu-uni.de>
2025-11-12 15:07:08 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,7 +3,37 @@
 
 #include "Animal.h"
 
-//define mammal class here
+class Mammal: {
+private:
+    string furColor;
+
+public:
+    Mammal(string n, int a, bool hungry, string color)
+        : Animal(n, a, hungry), furColor(color) {}
+
+    void display( {
+        cout << "Animal Type: Mammal" << endl;
+        Animal::display();
+      
+    }
+};
+
+
+class Bird : public Animal {
+private:
+    float wingSpan;
+
+public:
+    Bird(string n, int a, bool hungry, float ws)
+        : Animal(n, a, hungry), wingSpan(ws) {}
+
+    void display()   {
+        cout << "Animal Type: Bird" << endl;
+        Animal::display();
+        cout << "Wing Span: " << wingSpan << " meters" << endl;
+        
+    }
+};
 
 
 
