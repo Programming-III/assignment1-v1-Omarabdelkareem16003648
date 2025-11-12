b9b7bbb2b6a620004423a4040f8f47d486c40b33
Omarabdelkareem16003648 <omar.gado@student.giu-uni.de>
2025-11-12 15:15:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,15 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
+    int main() {
+   
+
     
+    Animal* tiger = new Mammal("Leo the tiger");
+    Animal* owl = new Bird("omar the owl");
+    Animal* snake = new Reptile("ahmed the Snake" );
+
+   
     
     return 0;
 }
